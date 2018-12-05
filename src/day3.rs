7f7d9e26df1213b5c528dd3894d49ef08f9a14a9
Justin Liew <justinliew@gmail.com>
2018-12-04 23:49:41 -0800

    day3

--- src/day3.rs
+++ src/day3.rs
@@ -11,11 +11,8 @@ pub fn entry() {
     _f.read_to_string(&mut _contents)
     .expect("something went wrong reading the file");
 
-    const MAX : usize = 1400;
-    const DEPTH : usize = 1;
-    // I need this to be a list of all overlaps
+    const MAX : usize = 1000;
     let mut m = [[0i32; MAX]; MAX];
-    let mut l = HashMap::new();
 
     lazy_static! {
         static ref RE: Regex = Regex::new(r"#([\d]*) @ ([\d]*),([\d]*): ([\d]*)x([\d]*)").unwrap();
@@ -28,14 +25,31 @@ pub fn entry() {
         let height = g[5].parse::<usize>().ok().unwrap();
         for w in 0..width {
             for h in 0..height {
-                if m[x+w as usize][y+h as usize] == 0 {
-                    m[x+w as usize][y+h as usize] = id;
-                }
+                m[x+w as usize][y+h as usize] += 1;
             }
         }
     }
 
-    for (k,v) in &l {
-        println!("{} {}", k,v);
+    for g in RE.captures_iter(&_contents) {
+        let id = g[1].parse::<i32>().ok().unwrap();
+        let x = g[2].parse::<usize>().ok().unwrap();
+        let y = g[3].parse::<usize>().ok().unwrap();
+        let width = g[4].parse::<usize>().ok().unwrap();
+        let height = g[5].parse::<usize>().ok().unwrap();
+        let mut found = false;
+        for w in 0..width {
+            for h in 0..height {
+                if m[x+w as usize][y+h as usize] != 1 {
+                    found = true;
+                    break;
+                }
+            }
+            if found {
+                break;
+            }
+        }
+        if !found {
+            println!("{}", id);
+        }
     }
 }
