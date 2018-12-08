30f6a79cf99f0f0344a7504d627e26964a63293b
Justin Liew <justinliew@gmail.com>
2018-12-07 18:00:56 -0800

    day 5 part 1

--- src/day5.rs
+++ src/day5.rs
@@ -7,34 +7,37 @@ fn test_polymer(c1 : char, c2 : char) -> bool {
 }
 
 pub fn entry() {
-    let mut f = File::open("day5-3.txt").expect("file not found");
+    let mut f = File::open("day5.txt").expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
     .expect("something went wrong reading the file");
 
-    let chars : Vec<char> = contents.chars().collect();
+    let mut chars : Vec<char> = contents.chars().collect();
 
     let mut i1 = 0;
     let mut i2 = 1;
     let mut total = chars.len();
     loop {
-        // TODO - we can't have 2 lowercase or 2 uppercase, so we have to check casing here
-        if test_polymer(chars[i1], chars[i2]) {
-            i1 -= 1;
-            i2 += 1;
+        if test_polymer(chars[i1],chars[i2]) {
+            chars.remove(i2);
+            chars.remove(i1);
+//            let s: String = chars.iter().collect();
+//            println!("{} (removing {},{})", s,i1,i2);
             total -= 2;
-        } else {
-            if i2 - i1 > 1 {
-                i2 += 2;
-                i1 = i2-1;
-            } else {
-                i1 += 2;
-                i2 += 2;
+            if i1 > 0 {
+                i1 -=1;
             }
+            if i2 > 1 {
+                i2 -=1;
+            }
+        } else {
+            i1 += 1;
+            i2 += 1;
         }
-        if i1 >= chars.len() || i2 >= chars.len() {
+        if i1 >= chars.len() || i2 >= chars.len () {
             break;
         }
     }
+
     println!("Total {}", total);
 }
