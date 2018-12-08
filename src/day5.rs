a409d769a317d1f002e6375226205e41f53065d9
Justin Liew <justinliew@gmail.com>
2018-12-07 16:27:42 -0800

    start of full test case as example first pass need to differentiate between casing

--- src/day5.rs
+++ src/day5.rs
@@ -1,20 +1,25 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+// 97-65
+fn test_polymer(c1 : char, c2 : char) -> bool {
+    (c1 as u8)-32 == c2 as u8 || (c2 as u8)-32 == c1 as u8
+}
+
 pub fn entry() {
-    let mut f = File::open("day5-2.txt").expect("file not found");
+    let mut f = File::open("day5-3.txt").expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
     .expect("something went wrong reading the file");
 
-    contents = contents.to_lowercase();
     let chars : Vec<char> = contents.chars().collect();
 
     let mut i1 = 0;
     let mut i2 = 1;
     let mut total = chars.len();
     loop {
-        if chars[i1] == chars[i2] {
+        // TODO - we can't have 2 lowercase or 2 uppercase, so we have to check casing here
+        if test_polymer(chars[i1], chars[i2]) {
             i1 -= 1;
             i2 += 1;
             total -= 2;
