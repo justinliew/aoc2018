abd56ecb0b9faa6c6e127e2c637dfa2ddff71bb7
Justin Liew <justinliew@gmail.com>
2018-12-11 21:27:12 -0800

    day 7 part 1 in go

--- day7.go
+++ day7.go
@@ -5,7 +5,22 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sort"
 )
+
+func SliceUniqMap(s []string) []string {
+	seen := make(map[string]struct{}, len(s))
+	j := 0
+	for _, v := range s {
+		if _, ok := seen[v]; ok {
+			continue
+		}
+		seen[v] = struct{}{}
+		s[j] = v
+		j++
+	}
+	return s[:j]
+}
 
 func main() {
 	file, err := os.Open("day7.txt")
@@ -17,11 +32,57 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	graph := make(map[string][]string)
 	deps := make(map[string][]string)
+	all := make(map[string]bool)
 	for scanner.Scan() {
 		// Step Q must be finished before step O can begin.
 		text := scanner.Text()
 		tokens := strings.Split(text, " ")
-		graph[tokens[0]] = append(graph[tokens[0]], tokens[7])
-		deps[tokens[7]] = append(deps[tokens[7]], tokens[0])
+		graph[tokens[1]] = append(graph[tokens[1]], tokens[7])
+		deps[tokens[7]] = append(deps[tokens[7]], tokens[1])
+		all[tokens[1]] = true
+		all[tokens[7]] = true
 	}
+
+	log.Printf("Graph")
+	for k,v := range graph {
+		log.Printf("%s -> %s", k,v)
+	}
+
+	var frontier []string
+
+	for k := range all {
+		_,ok := deps[k]
+		if !ok {
+			frontier = append(frontier,k)
+		}
+	}
+	
+	var output []string
+	for ;len(frontier) > 0;{
+		frontier = SliceUniqMap(frontier)
+		sort.Strings(frontier)
+		log.Printf("Frontier is %v", frontier)
+		found := false
+		for findIndex := 0; !found;findIndex = findIndex + 1 {
+			candidate := frontier[findIndex]
+			d,ok := deps[candidate]
+			if !ok || len(d) == 0 {
+				log.Printf("Adding %s", candidate)
+				output = append(output,candidate)
+				for _,removeDep := range graph[candidate] {
+					for dIndex := range deps[removeDep] {
+						if deps[removeDep][dIndex] == candidate {
+							deps[removeDep] = append(deps[removeDep][:dIndex],deps[removeDep][dIndex+1:]...)
+							break
+						}
+					}
+				}
+				frontier = append(frontier[:findIndex],frontier[findIndex+1:]...)
+				frontier = append(frontier,graph[candidate]...)
+				findIndex = 0
+				found = true
+			}
+		}
+	}
+	log.Printf("%v", output)
 }
